0a6607d67a247c8b44b72409399816013c830d55
Sudhanshu <perfectsudh@gmail.com>
2021-05-07 09:28:00 +0530

    update base-url

--- utils/api-routes.ts
+++ utils/api-routes.ts
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_SERVER || "http://192.168.1.7:8080"
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_SERVER || "http://192.168.1.20:8080"
 
 const auth = {
   SIGNIN: `${BASE_URL}/api/v1/auth/login`,
